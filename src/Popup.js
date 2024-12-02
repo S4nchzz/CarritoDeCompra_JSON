771213acc04202560a757858902c5d07f84ecc3b
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-12-02 20:37:31 +0100

    Se ha añadido un contador de representando el total de dinero que se lleva en el carrito

--- src/Popup.js
+++ src/Popup.js
@@ -1,15 +1,31 @@
-function popUp({ list, clearArticles }) {
+import { useEffect, useState } from "react";
+
+function PopUp({ list, clearArticles }) {
     const productList = list.map(art => (
         <p key={art.id}>{art.nombre}</p>
-      ));
+    ));
+    
+    const [total, setTotal] = useState(0)
+
+
+    useEffect(() => {
+        let sum = 0
+        list.forEach(element => {
+            sum = sum + element.precio
+        });
+
+        setTotal(sum)
+    }, [list])
+
 
     return (
         <div className="popUp">
             <h1>Carrito</h1>
             {productList}
+            {total}
             <button onClick={() => clearArticles()}>Clear</button>
         </div>
     )
 }
 
-export default popUp;
+export default PopUp;
